e49cacf54d5c336dec07bf80aa05baec98b81c15
tenzin kalden <tkalden61@gmail.com>
2024-03-14 23:15:19 +0530

    navbar and hero changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import dalai_lama from "/Users/tenzinkalden/Desktop/Scholar/ScholarshipProject/src/dalai_lama.png"
+import cloud from "/Users/tenzinkalden/Desktop/Scholar/ScholarshipProject/src/tibetan_cloud.png"
 
 const Hero = () => {
-  return <div>hero</div>;
+  return (
+    <div className="dalai_main">
+      
+      <div className="contain">
+        <img className="cloud" src={cloud} alt="cloud"/>
+        <p className="con_words">“The most important use of knowledge and education is to help us understand the importance of engaging in more wholesome actions and bringing about discipline within our minds.”</p>
+        <img className="dalai_lama" src={dalai_lama} alt="dalai_lama"/>
+        
+        <ul className="name">
+          <li>His Holiness 14th Dalai Lama</li>
+        </ul>
+        <img className="cloud2" src={cloud} alt="cloud"/>
+        <p className="cont_base" >scholarships are not just available to students who are academically gifted or have achieved perfect exam scores. </p>
+      </div>
+      
+    </div>);
 };
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+  <div>
+    <div className="nav_main">
+      <a className="kunzoe" href="">Kunzoe</a>
+      <ul className="nav_list">
+        <li><a href="">Scholarship type</a></li>
+        <li><a href="">Admission Guidance</a></li>
+        <li><a href="">Student Blogs</a></li>
+        <li><a href="">About Us</a></li>
+      </ul>
+    </div>
+  </div>);
 };
 
 export default Navbar;
