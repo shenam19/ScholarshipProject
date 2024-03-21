b477f1513efe41a219958f78c2dba260b708aa63
shenam19 <shenam19@gmail.com>
2024-03-21 11:20:56 +0530

    DLTS page added

---
+++ src/components/DalaiLamaTrustScholarship.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+const DalaiLamaTrustScholarship = () => {
+  return (
+    <>
+      <div className="header">
+        <h1>THE DALAI LAMA TRUST SCHOLARSHIP 2023</h1>
+        <p>
+          The Dalai Lama Trust is delighted to announce ‘The Dalai Lama Trust
+          Scholarship’ for the year 2023. This scholarship is fully funded and
+          administered by The Dalai Lama Trust, Office of His Holiness the Dalai
+          Lama.
+        </p>
+        <p>
+          some important requirements for this scholarship were mentioned below.
+          to know more please click the link for further informations.
+        </p>
+      </div>
+      <div className="middle">
+        <h1 className="types">types of scholarships</h1>
+        <div className="sideText">
+          <h2>A. UNDERGRADUATE SCHOLARSHIPS</h2>
+          <p>
+            100 scholarships for the Undergraduate or Diploma Program of minimum
+            3 years.
+          </p>
+          <h2>B. POSTGRADUATE SCHOLARSHIPS</h2>
+          <p>5 scholarships to the Postgraduate Program.</p>
+        </div>
+      </div>
+      <div className="end">
+        <h1>deadlines</h1>
+        <p>
+          Eligible candidates must apply online by May 31st, 2023 for
+          Undergraduate scholarship and June 30th, which has been extended
+          to August 31st, 2023 for Postgraduate scholarship. Applications
+          received after the deadline and incomplete application and/or
+          documentation will be disqualified. 
+        </p>
+        <h1>documents you needed</h1>
+        <h2>Undergraduate Scholarship</h2>
+        <p> A. GB (front and back)</p>
+        <p> B. RC</p>
+        <p> C. Class X Mark sheet</p>
+        <p> D. Class XII Mark sheet</p>
+        <p>E. Best Boy/Girl Certificate if applicable.</p>
+        <p> F. Passport photo</p>
+        <p> G.  Duly filled “Form-A” and/or “Form-B” where required</p>
+        <p>
+          {" "}
+          Please compile all documents except passport photo into a single PDF
+          file. Please check our sample document file for the required format.
+        </p>
+        <h2>Postgraduate Scholarship</h2>
+        <p>A. GB (front and back)</p>
+        <p>B. RC</p>
+        <p>
+          C. Undergraduate Mark sheet (in chronological order of each year of
+          course)
+        </p>
+        <p>
+          {" "}
+          D. Provisional Degree Certificate or Degree Certificate if available
+        </p>
+        <p>E. Internship completion certificate where applicable</p>
+        <p>F. Passport photo</p>
+
+        <p>
+          Please compile all documents except passport photo into a single PDF
+          file. Please check our sample document file for the required format.
+        </p>
+        <button type="button">
+          visit dalai lama trust website for more info
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default DalaiLamaTrustScholarship;
